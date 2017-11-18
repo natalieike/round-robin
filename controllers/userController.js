9b0ec99e9d08fed2b908ee2f08c3842590555ea2
Natalie Ike <natalieike@yahoo.com>
2017-11-18 13:03:08 -0800

    Create User API Completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,7 @@ module.exports = {
 	lastName
 	streetAddress
 	country - own table
-	stateProvice - own table
+	stateProvnice - own table
 	city
 	postalCode
 	aboutMe - can be null
@@ -18,7 +18,54 @@ module.exports = {
 }
 */
   create: function(req, res) {
-    
+    db.country.findOrCreate({
+    	where: {
+    		countryName: req.body.country
+    	},
+    	defaults: {
+    		countryName: req.body.country
+    	}
+    }).spread(function(country, created){
+    	db.stateProvince.findOrCreate({
+    		where: {
+    			stateProvinceName: req.body.stateProvince
+    		},
+    		defaults:{
+    			stateProvinceName: req.body.stateProvince
+    		}
+    	}).spread(function(stateProvince, cr){
+    		request = req.body;
+    		db.user.create({
+    			firstName: request.firstName,
+    			lastName: request.lastName,
+    			streetAddress: request.streetAddress,
+    			city: request.city,
+    			postalCode: request.postalCode,
+    			aboutMe: request.aboutMe,
+    			shippingPreferenceId: request.shippingPreferenceId,
+    			fbUserId: request.fbUserId,
+    			oAuthToken: request.oAuthToken,
+    			stateProvinceId: stateProvince.id
+    		}).then(function(user){
+    			req.body.interestsFandoms.map(interest => {
+    				db.interestsFandoms.findOrCreate({
+    					where: {
+    						description: interest
+    					},
+    					defaults: {
+    						description: interest
+    					}
+    				}).spread(function(intFan, created){
+    					db.interestsFandomsAssociations.create({
+    						interestsFandomId: intFan.dataValues.id,
+    						userId: user.id
+    					});
+    				});
+    			});
+					res.json(user);
+    		});
+    	});
+    }).catch(err => res.json.err);
   },
   findAll: function(req, res){
 
