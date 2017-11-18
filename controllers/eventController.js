71d947a7f5ac7374f21b8378767b6f47b836a1a7
Natalie Ike <natalieike@yahoo.com>
2017-11-18 11:53:29 -0800

    API Setup, User Model fix

---
+++ controllers/eventController.js
@@ -0,0 +1,20 @@
+const db = require("../models");
+
+// Defining methods for the eventController
+module.exports = {
+  create: function(req, res) {
+    
+  },
+  findAll: function(req, res){
+
+  },
+  findById: function(req, res){
+
+  },
+  update: function(req, res) {
+    
+  },
+  remove: function(req, res) {
+
+  }  
+};
---
+++ controllers/userController.js
@@ -0,0 +1,35 @@
+const db = require("../models");
+
+// Defining methods for the userController
+module.exports = {
+/* Req = {
+	firstName
+	lastName
+	streetAddress
+	country - own table
+	stateProvice - own table
+	city
+	postalCode
+	aboutMe - can be null
+	interestsFandoms = [] - own table
+	shippingPreferenceId (Assume Shipping Pref table already populated)
+	fbUserId
+	oAuthToken
+}
+*/
+  create: function(req, res) {
+    
+  },
+  findAll: function(req, res){
+
+  },
+  findById: function(req, res){
+
+  },
+  update: function(req, res) {
+    
+  },
+  remove: function(req, res) {
+
+  }  
+};
