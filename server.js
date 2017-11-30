d4a429571ace49ea72bf5b2b22b8ac84b847a6c0
Natalie Ike <natalieike@yahoo.com>
2017-11-29 22:10:44 -0800

    heroku debug

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ db.sequelize.sync({ force: false }).then(function(){
 
 	// Send every request to the React app
 	// Define any API routes before this runs
-	
+	/*
 	app.get("*", function(req, res) {
 	  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 	});
-	
+	*/
 
 	app.listen(PORT, function() {
 	  console.log(`🌎 ==> Server now on port ${PORT}!`);
