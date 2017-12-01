023dd42fb8d4dd1121263dda54214b964f123186
Natalie Ike <natalieike@yahoo.com>
2017-11-30 20:05:43 -0800

    re-locate login button so it's always present

--- client/src/components/Navpills.js
+++ client/src/components/Navpills.js
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 
 const Navpills = () =>
   <ul className="nav nav-pills nav-justified">
+    <li>
+      <div className="fbbutton">
+        <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="true"></div>
+      </div>
+    </li>
     <li className={window.location.pathname === "/home" ? "active" : ""}>
       <Link to="/home">Home</Link>
     </li>
--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -16,9 +16,7 @@ class Home extends Component {
 			      <li>Participate in Swaps - Private Swaps by Invite, or Search for Public Events</li>
 			    </ul>
 		      <h3 className="text-center">Sign in with Facebook to Get Started!</h3>
-		      <div className="text-center">
-		      	<div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="true"></div>
-		      </div>
+
 		    </div>
 			</div>);
   };
